70bb5a8e101d7513a410749af13ce044cde8d410
JelenaFjodorova <fjodorovaa.lena@gmail.com>
2017-09-24 23:20:14 +0300

    Add files via upload

---
+++ test/jelenaweatherforecast/JelenaWeatherForecastTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jelenaweatherforecast;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jelena
+ */
+public class JelenaWeatherForecastTest {
+    
+
+    @Test
+    public void testGetCurrentForecast() {
+        fail("The test case is a prototype.");
+    }
+
+
+    @Test
+    public void testGetThreeDaysForecast() {
+        fail("The test case is a prototype.");
+    }
+
+
+    @Test
+    public void testGetHighestTemp() {
+        fail("The test case is a prototype.");
+    }
+
+
+    @Test
+    public void testGetLowestTemp() {
+        fail("The test case is a prototype.");
+    }
+
+
+    @Test
+    public void testGetGeoCoordinates() {
+        fail("The test case is a prototype.");
+    }
+    
+}
